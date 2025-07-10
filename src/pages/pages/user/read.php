7e826db3ab1feb89e834f584b2a7c30f41a14984
Wanderson <wanderson991.wb@gmail.com>
2025-07-09 21:38:41 -0300

    add read

--- src/pages/pages/user/read.php
+++ src/pages/pages/user/read.php
@@ -16,4 +16,26 @@ $users = readUserAction($conn);
   <div class="row flex-center">
     <?php if(isset($_GET["message"])) echo(printMessage($_GET["message"])); ?>
   </div>
+
+  <table class="table-users">
+    <tr>
+      <th>NAME</th>
+      <th>EMAIL</th>
+      <th>PHONE</th>
+    </tr>
+    <?php foreach($users as $row): ?>
+    <tr>
+      <td class="user-name"><?=htmlspecialchars($row["name"])?></td>
+      <td class="user-email"><?=htmlspecialchars($row["email"])?></td>
+      <td class="user-phone"><?=htmlspecialchars($row["phone"])?></td>
+      <td>
+        <a class="btn btn-primary text-white" href="./edit.php?id=<?=$row["id"]?>">Edit</a>
+      </td>
+      <td>
+        <a class="btn btn-danger text-white" href="./delete.php?id=<?=$row["id"]?>"></a>
+      </td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
 </div>
+<?php require_once "../partials/footer.php"; ?>
